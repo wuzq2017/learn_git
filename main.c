025489e1f763efc6b060d6998c31443e8a0eb28f
wuzq <wdd_0227@163.com>
2022-06-08 09:44:12 +0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+int sum(int a, int b)
+{
+  return a+b;
+}
+typedef struct {
+  char name[255];
+  unsigned int age;
+  /* boolean sex; */
+}student_info_t;
 void main(void)
 {
-	printf("hello\n");
+  int sum1;
+  sum1 = sum(1,3);
+  printf("%d\n",sum1);
+  printf("hello\n");
 }
